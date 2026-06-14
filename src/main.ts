392ac6b2a3bc6e88c3c65f76e71cc69a804ff9d9
Jacob Slunga <jacobslunga21@yahoo.se>
2026-06-14 21:12:21 +0200

    updated font and colors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import './style.css'
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+import "./style.css";
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 <main id="content">
   <h1>Jacob Slunga</h1>
   <p>I'm building <a href="https://liutentor.se" target="_blank">LiU Tentor</a>; an AI-powered exam search engine helping students at Linköping University study smarter.</p>
   <p>In the past, I've enjoyed working across fullstack, embedded, and ML at <a href="https://www.axis.com" target="_blank">Axis Communications</a>, and writing my bachelor thesis at <a href="https://www.saab.com" target="_blank">Saab</a> on visualizing complex system requirements. I study CS at LiU.</p>
   <p>Connect with me on <a href="https://github.com/jacobslunga" target="_blank">github</a>, <a href="https://www.linkedin.com/in/jacob-slunga-9121131a2/" target="_blank">linkedin</a>, or at <a href="mailto:jacobslunga21@yahoo.se">jacobslunga21@yahoo.se</a>!</p>
 </main>
-`
+`;
